7443c8acc966efa6627c0a0ae37424bbb9a43d84
hColleen <hcolleen@gmail.com>
2018-11-18 09:51:41 -0700

    add unit description

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import './App.css'
 import Description from './components/Description';
 import Community from './components/Community';
+import Units from './components/Units';
 
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <Header />
         <Description />
         <Community />
+        <Units />
       </div>
     );
   }
