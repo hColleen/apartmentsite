95c32522909ec6775a3bb2469286c4696520140d
hColleen <hcolleen@gmail.com>
2018-11-18 15:17:50 -0700

    update test text to better lengths for demonstration

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Header extends Component{
     render(){
         return(
-            <h1 className = "communityName">Lick yarn when guests come over</h1>
+            <h1 className = "communityName">Totam</h1>
         )
     }
 }
