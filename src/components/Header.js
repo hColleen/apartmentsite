7268947fc0290e669e47b83285bb649ee7f837cc
hColleen <hcolleen@gmail.com>
2018-11-18 09:31:47 -0700

    update header, remove default information

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component{
+    render(){
+        return(
+            <h1 className = "communityName">Lick yarn when guests come over</h1>
+        )
+    }
+}
