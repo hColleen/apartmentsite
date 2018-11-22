8d1f23b8af16a5a708019c3f222f2b868b96bfa5
hColleen <hcolleen@gmail.com>
2018-11-22 03:57:43 -0700

    description setup

--- src/components/Community.js
+++ src/components/Community.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Community extends Component {
     render() {
         return (
-            <div className = "communityDescription">
+            <div className = "community">
             <p>Quis eius. Magnam voluptatem so officia or ab. Laborum nihil so laborum, so ipsam voluptas and fugit. Architecto excepteur, tempor yet inventore quia, quisquam rem. Enim adipisci. Quaerat fugiat and exercitationem. Totam. Et modi but nihil and irure magni yet consequuntur. Nulla ratione yet magnam or aspernatur so iste so laborum. Consectetur doloremque for sed or laudantium but doloremque. Nostrud aliquam, and sint or adipisicing and architecto, si and nesciunt. Exercitationem voluptate ea corporis so sequi ipsa. Accusantium natus cupidatat, iure or eu so do. Fugit architecto for eos, dolorem for cupidatat dolor so tempora. Adipisicing in yet magnam for dolores, but et but quis for aliqua. Eum. Quaerat omnis so beatae natus id. Esse voluptate nostrud for voluptatem quae voluptatem consectetur. Magni magna irure. </p>
             </div>
         )
--- src/components/Description.js
+++ src/components/Description.js
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Balcony from '../images/balcony.jpg'
 
 export default class Description extends Component {
     render() {
         return (
             <div className="communityDescription">
+            <img
+                src = {Balcony}
+                alt = "Balcony and trees"
+                className = "communityImage"
+                />
                 <p>Cat ipsum dolor sit amet, nisi. Ipsum commodo but nisi but natus. Excepteur pariatur numquam tempora fugiat laboriosam and sunt. Quisquam. Ex. Ratione ab exercitation ullamco and ut. Anim unde occaecat, ipsum inventore magna dolorem. </p>
+                
             </div>
         )
     }
