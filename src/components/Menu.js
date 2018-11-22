855498c23ffd549a480413f9ea0928769c80082e
hColleen <hcolleen@gmail.com>
2018-11-21 17:09:47 -0700

    update menu styling and coloring

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,26 +3,23 @@ import React, { Component } from 'react'
 export default class Menu extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar w/ text</a>
+            <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: `rgba(0, 134, 179, 0.8)`}}>
+                <a className="navbar-brand" href="#">Totam</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarText">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            <a className="nav-link" href="#">Amenities</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
+                            <a className="nav-link" href="#">Floor Plans</a>
                         </li>
                     </ul>
-                    <span className="navbar-text">
-                        Navbar text with an inline element
-    </span>
                 </div>
             </nav>
         )
